06b5ace0839e2a4864833313e41b02de32f15b0f
Amii <meganar7777@gmail.com>
2023-05-20 18:23:14 -0500

    Agrega formulario dinamico

--- assets/scripts/formulario.js
+++ assets/scripts/formulario.js
@@ -1,29 +1,78 @@
 "use strict";
 
-const $formulario = document.getElementById('formulario');
+const $formulario = document.getElementById("formulario");
 
 const formData = {
-    nombre: null,
-    apellidos: null,
-    fechaNacimiento: null,
-    email: null,
-    tel: null,
-    categoria: null,
-    mensaje: null
+  nombre: null,
+  apellidos: null,
+  fechaNacimiento: null,
+  correo: null,
+  tel: null,
+  categoria: null,
+  mensaje: null
+};
+
+for (const key in formData) {
+  $formulario[key].addEventListener("input", (e) => {
+    removeErrorMessage(e.target);
+    if (e.target.id === "tel") {
+      e.target.value = e.target.value.replace(/\D/g, "").slice(0, 10);
+    }
+  });
 }
 
-$formulario.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    formData.nombre = $formulario.nombre.value;
-    formData.apellidos = $formulario.apellidos.value;
-    formData.fechaNacimiento = $formulario.fechaNacimiento.value;
-    formData.nombre = $formulario.nombre.value;
-    formData.nombre = $formulario.nombre.value;
-    formData.nombre = $formulario.nombre.value;
-    formData.nombre = $formulario.nombre.value;
-
-    $formulario.reset();
-
-    console.log(formData);
+$formulario.fechaNacimiento.addEventListener("change", (e) => {
+  if (getAge(e.target.value) < 18)
+    addErrorMessage(
+      e.target,
+      "Tienes que ser mayor de edad para enviar este formulario",
+      true
+    );
 });
+
+$formulario.correo.addEventListener("change", (e) => {
+  if (!(e.target.value.includes("@") && e.target.value.includes(".com")))
+    addErrorMessage(e.target, "Direccion de correo no valida", true);
+});
+
+$formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  for (const key in formData) {
+    removeErrorMessage($formulario[key]);
+    let alreadyScrolled = false;
+
+    if ($formulario[key].value === "") {
+      addErrorMessage(
+        $formulario[key],
+        "Este campo es obligatorio",
+        !alreadyScrolled
+      );
+      if (!alreadyScrolled) alreadyScrolled = true;
+    }
+  }
+
+  if (document.getElementsByClassName("errField").length === 0)
+    document.getElementById("thanksMsg").className = "thanksVisible";
+});
+
+function getAge(bDate) {
+  return new Date().getFullYear() - new Date(bDate).getFullYear();
+}
+
+function addErrorMessage(element, msg, scrollTo) {
+  const errMessage = document.createElement("p");
+  errMessage.textContent = msg;
+  errMessage.className = "errMsg";
+
+  element.parentElement.insertBefore(errMessage, element);
+  if (scrollTo) element.parentElement.scrollIntoView();
+
+  element.classList.add("errField");
+}
+
+function removeErrorMessage(target) {
+  target.classList.remove("errField");
+  const errMsg = target.parentElement.getElementsByClassName("errMsg")[0];
+  if (errMsg != null) errMsg.remove();
+}
