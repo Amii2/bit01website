99ec47a224046de0929807bc1ad428e27e7061f4
Amii <meganar7777@gmail.com>
2023-05-23 14:02:28 -0500

    Agrega boton para limpiar el imput, arregla estilos de formulario y organiza codigo formulario (JavaScript)

--- assets/scripts/formulario.js
+++ assets/scripts/formulario.js
@@ -2,6 +2,18 @@
 
 const $formulario = document.getElementById("formulario");
 
+const clearButton = document.createElement("span");
+clearButton.style =
+  "position: absolute; bottom: 2px; pointer-events: auto; cursor: pointer; font-weight: bold;";
+clearButton.addEventListener("mousedown", (e) => {
+  e.preventDefault();
+  const element = clearButton.parentElement.getElementsByTagName(
+    document.activeElement.tagName
+  )[0];
+  element.value = "";
+  clearButton.remove();
+});
+
 const formData = {
   nombre: null,
   apellidos: null,
@@ -13,14 +25,31 @@ const formData = {
 };
 
 for (const key in formData) {
-  if (key === "fechaNacimiento") continue;
+  if (key === "fechaNacimiento" || key === "categoria") continue;
+
   $formulario[key].addEventListener("input", (e) => {
     removeErrorMessage(e.target);
-    if (e.target.id === "tel") {
-      e.target.value = e.target.value.replace(/\D/g, "").slice(0, 10);
-    }
+    if (key !== "tel") addClearButton(key, e.target);
+  });
+
+  $formulario[key].addEventListener("focus", (e) => {
+    addClearButton(key, e.target);
+  });
+
+  $formulario[key].addEventListener("blur", (e) => {
+    clearButton.remove();
   });
 }
+
+$formulario.tel.addEventListener("input", (e) => {
+  const number = "0123456789";
+  e.target.value = e.target.value
+    .split("")
+    .filter((char) => number.includes(char))
+    .slice(0, 10)
+    .join("");
+  addClearButton("tel", e.target);
+});
 
 $formulario.fechaNacimiento.addEventListener("change", (e) => {
   removeErrorMessage(e.target);
@@ -30,6 +59,10 @@ $formulario.fechaNacimiento.addEventListener("change", (e) => {
       "Tienes que ser mayor de edad para enviar este formulario",
       true
     );
+});
+
+$formulario.categoria.addEventListener("change", (e) => {
+  removeErrorMessage(e.target);
 });
 
 $formulario.correo.addEventListener("change", (e) => {
@@ -84,6 +117,23 @@ document
         .getElementsByTagName("h2")[0].textContent = "¡Gracias Nombre!";
     }, 300);
   });
+
+function addClearButton(key, target) {
+  if (target.value === "") {
+    clearButton.remove();
+    return;
+  }
+
+  if (key === "mensaje") {
+    clearButton.innerHTML = "clear";
+    clearButton.style.setProperty("left", "88%");
+  } else {
+    clearButton.innerHTML = "x";
+    clearButton.style.setProperty("left", "71%");
+  }
+
+  target.after(clearButton);
+}
 
 function getAge(bDate) {
   return new Date().getFullYear() - new Date(bDate).getFullYear();
